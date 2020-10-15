9a1fe2387814c4f2d13aa9ca14b03efd52810961
Diego Lucas <diego.lucasilva@gmail.com>
2020-10-15 19:35:11 -0300

    info

--- src/com/dls/structural/composite/Main.java
+++ src/com/dls/structural/composite/Main.java
@@ -1,6 +1,7 @@
 package com.dls.structural.composite;
 /*
-The composite pattern is meant to allow treating individual objects and compositions of objects, or “composites” in the same way.
+The composite pattern is meant to allow treating individual objects and compositions of objects, or “composites”
+in the same way.
 
 It can be viewed as a tree structure made up of types that inherit a base type,
 and it can represent a single part or a whole hierarchy of objects.
